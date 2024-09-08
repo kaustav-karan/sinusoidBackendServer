31794b3871e7cee7f797dc51e69dd0aa4d9e551b
Kinshukaumhum <kaustavkaran2015@gmail.com>
2024-09-09 01:17:43 +0530

    refactor: Add deleteEvent route to eventRoutes.js

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -5,6 +5,7 @@ const {
   getEventById,
   createEvent,
   updateEvent,
+  deleteEvent,
 } = require("../controllers/eventController");
 
 router.get("/events", getAllEvents);
@@ -14,5 +15,7 @@ router.get("/events/:eventId", getEventById);
 router.post("/events", createEvent);
 
 router.put("/events/:eventId", updateEvent);
+
+router.delete("/events/:eventId", deleteEvent);
 
 module.exports = router;
