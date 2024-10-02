0a60953fb56fa3d519fefbf2227e6365bb79063b
Kaustav <kaustavkaran2015@gmail.com>
2024-10-02 11:56:47 +0530

    Refactor merchandiseOrderSchema.js to remove default value for _id field

--- modal/pages/merchandiseOrderSchema.js
+++ modal/pages/merchandiseOrderSchema.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const uuid = require("uuid4");
 
 const merchandiseOrderSchema = mongoose.Schema({
-  _id: { type: String, required: true, default: uuid() },
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
