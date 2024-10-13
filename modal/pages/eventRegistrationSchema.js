e5830705ec1a6953d76efef2c88bfa229de343e8
Kaustav <kaustavkaran2015@gmail.com>
2024-10-14 02:20:33 +0530

    Refactor eventRegistrationSchema.js to include team members field

--- modal/pages/eventRegistrationSchema.js
+++ modal/pages/eventRegistrationSchema.js
@@ -28,27 +28,32 @@ const mongoose = require("mongoose");
 // });
 
 const eventRegistrationSchema = mongoose.Schema({
-    registrationId: { type: String, required: true },
-    eventId: { type: String, required: true },
-    eventParticipants: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    universityName: { type: String, required: true },
-    isNiitStudent: { type: Boolean, required: true },
-    teamMembers: { type: Number, required: true },
-    teamDetails: [{
-        teamMemberName: { 
-            type: String, 
-            required: function() { return this.teamDetails.length > 0; } 
-        },
-        teamMemberEmail: { 
-            type: String, 
-            required: function() { return this.teamDetails.length > 0; } 
-        }
-    }]
+  registrationId: { type: String, required: true },
+  eventId: { type: String, required: true },
+  eventParticipants: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  universityName: { type: String, required: true },
+  isNiitStudent: { type: Boolean, required: true },
+  teamMembers: { type: Number, required: true },
+  teamDetails: [
+    {
+      teamMemberName: {
+        type: String,
+        required: false,
+      },
+      teamMemberEmail: {
+        type: String,
+        required: false,
+      },
+    },
+  ],
 });
 
-const eventRegistrationModal = mongoose.model("eventRegistrations", eventRegistrationSchema);
+const eventRegistrationModal = mongoose.model(
+  "eventRegistrations",
+  eventRegistrationSchema
+);
 module.exports = eventRegistrationModal;
